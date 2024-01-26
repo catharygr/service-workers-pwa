b25beaf6a829e029666f29f2f1a068676014767f
catharygr <103951447+catharygr@users.noreply.github.com>
2024-01-26 11:27:46 +0000

    Trabajando en el static styles del app.js

--- app/index.js
+++ app/index.js
@@ -3,6 +3,7 @@
 import { App } from "./src/js/app.js";
 import "./src/scss/guide.scss";
 import "./src/scss/first.scss";
+import "./src/scss/components.scss";
 
 customElements.define("my-app", App);
 document.getElementById("app").innerHTML = "<my-app></my-app>";
--- app/src/js/app.js
+++ app/src/js/app.js
@@ -1,21 +1,43 @@
 import { LitElement, html, css } from "lit";
 import * as TimerCompoent from "../../node_modules/@bubulazi/timer/dist/index";
+import * as logo from "../images/TimerPWA-logos_transparent.png";
 
 export class App extends LitElement {
+  static styles = css`
+    .logo {
+      max-width: 20rem;
+      margin: 0 auto;
+      display: block;
+    }
+
+    .main-container {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+    .inner-container {
+      width: clamp(16em, 90vw, 42rem);
+    }
+  `;
   connectedCallback() {
     super.connectedCallback();
     window.customElements.define("timer-component", TimerCompoent);
   }
   render() {
     return html`
-      <timer-component
-        shorttimer
-        doubledigits
-        btnpause
-        btnplay
-        btnreset
-        reverse
-      ></timer-component>
+      <main class="main-container">
+        <div class="inner-container">
+          <img class="logo" src="${logo.default}" alt="TimerPWA logo" />
+          <timer-component
+            shorttimer
+            doubledigits
+            btnpause
+            btnplay
+            btnreset
+            reverse
+          ></timer-component>
+        </div>
+      </main>
     `;
   }
 }
